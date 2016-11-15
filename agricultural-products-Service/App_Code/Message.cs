068b684743da74369ddabc7c618fefb6859904cb
Kevin <kelly10056040@gmail.com>
2016-11-16 01:29:01 +0800

    20161116_0128_change message

--- agricultural-products-Service/App_Code/Message.cs
+++ agricultural-products-Service/App_Code/Message.cs
@@ -10,7 +10,7 @@ public class Message
 {
     // 繁體中文
     public string success = "Success";
-    public string noData_cht = "查無資料";
+    public string noData_cht = "無此資料";
     public string contactError_cht = "連接錯誤";
     public string sqlError_cht = "伺服器指令錯誤";
     public string error_cht = "伺服器錯誤";
